e9ac9cac78ce446a9ba16ce342de46bb548be7fb
Filip Ekberg <mail@filipekberg.se>
2014-05-20 17:43:43 +1000

    Add project

---
+++ Src/MoveFilesToAzureStorage/AzureStorageHelper.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.IO;
+using Microsoft.WindowsAzure;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
+
+namespace MoveFilesToAzureStorage
+{
+    /*
+     * Thanks to Rakki Muthukumar 
+     * http://thenextdoorgeek.com/post/Windows-Azure-Websites-WebJob-to-upload-FREB-logs-to-Azure-Blob-Storage
+     */
+    public class AzureStorageHelper
+    {
+        private CloudStorageAccount storageAccount;
+        private CloudBlobClient blobClient;
+        private CloudBlobContainer container;
+        private CloudBlockBlob blockBlob;
+
+        private bool deleteAfterUpload = false;
+
+        public AzureStorageHelper()
+        {
+            Console.WriteLine("Using Storage Container " + Environment.GetEnvironmentVariable("APPSETTING_StorageContainer"));
+            storageAccount = CloudStorageAccount.Parse(Environment.GetEnvironmentVariable("CUSTOMCONNSTR_StorageConnectionString"));
+            blobClient = storageAccount.CreateCloudBlobClient();
+            container = blobClient.GetContainerReference(Environment.GetEnvironmentVariable("APPSETTING_StorageContainer"));
+            container.CreateIfNotExists();
+        }
+
+        public void UploadFileToBlob(string name, string path)
+        {
+            try
+            {
+                Console.WriteLine("Starting uploading " + name);
+                using (var fileStream = System.IO.File.OpenRead(path))
+                {
+                    blockBlob = container.GetBlockBlobReference(name);
+                    blockBlob.UploadFromStream(fileStream);
+                    Console.WriteLine(name + " successfully uploaded!");
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
---
+++ Src/MoveFilesToAzureStorage/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.WindowsAzure.Management.Storage;
+using System.IO;
+using System.Configuration;
+
+namespace MoveFilesToAzureStorage
+{
+    public class Program
+    {
+        private static AzureStorageHelper storageHelper;
+
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Initializing AzureStorageHelper!");
+
+            storageHelper = new AzureStorageHelper();
+
+            string path = Environment.GetEnvironmentVariable("APPSETTING_FilesLocation");
+            
+            Console.WriteLine("Loading files from " + path);
+
+            TraverseDirectory(new DirectoryInfo(path));
+
+            Console.WriteLine("AzureStorageHelper Done at " + DateTime.Now.ToString() + " Stopping!");
+        }
+
+        private static void TraverseDirectory(DirectoryInfo directory)
+        {
+            Console.WriteLine(String.Format("{0} Started working directory {1}", DateTime.Now.ToString(), directory.FullName));
+
+            if (directory == null) return;
+
+            foreach(var subDirectory in directory.GetDirectories())
+            {
+                TraverseDirectory(subDirectory);
+            }
+
+            foreach (var file in directory.GetFiles("*"))
+            {
+                Upload(file);
+            }
+        }
+
+        private static void Upload(FileInfo file)
+        {
+            Console.WriteLine("Creating " + file.Name + " " + file.FullName);
+            storageHelper.UploadFileToBlob(file.Name, file.FullName);
+        }
+    }
+}
